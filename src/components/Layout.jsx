ef44b70d3bcbeaa2ba299a10f2ec0934d6e2383a
Karim <karimsaadi.dev@gmail.com>
2024-09-05 17:39:52 +0400

    create layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,14 @@
-function Layout() {
-  return <div>Layout</div>;
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
